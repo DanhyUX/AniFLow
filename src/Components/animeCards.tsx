dd4faeeb4ee077a07ae5163c11de79f35a5106a9
Danhy <danhybnunez@hotmail.com>
2025-08-06 17:10:12 -0400

    Add AnimeCard component and refactor Home page to use it for rendering anime cards

---
+++ src/Components/animeCards.tsx
@@ -0,0 +1,32 @@
+import PlayBlack from '/assets/playblack.png'
+
+function AnimeCard({anime}:{anime: any}) {
+    const {size, link, image, title, rating, year, seasons, description, tags } = anime
+    return (
+        <div className={`bg-gray-700 lg:w-[${size}] h-[380px] rounded-2xl cursor-pointer relative group overflow-hidden flex flex-col justify-end pb-5`}>
+        <a href={link}> 
+            <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center z-10 opacity-0 hover:opacity-100 transition-opacity">
+            <img src={PlayBlack} alt="Play" className='py-4 px-4 bg-white rounded-[100%]' />
+            </div>
+        </a>
+        <img src={image} alt="" className="w-full h-full absolute top-0 left-0 object-cover opacity-40 rounded-2xl group-hover:scale-110 transition-transform" /> 
+        <div className='relative px-6'>
+            <p className='text-3xl font-bold'>{title}</p>
+        <div className='flex items-center gap-4 mt-3 mb-4'>
+        
+            <p className='bg-gray-600 py-1 px-3 rounded-[6px] '>{rating}</p>
+            <p>{year}</p>
+            <p>{seasons} Seasons</p>
+            </div>
+            <p>{description}</p>
+            <div className='flex gap-1 mt-4'>
+            {tags.map((tag, index) => (
+                <p className={`${index === 0? "bg-red-700" : "bg-gray-600"} py-1 px-3 rounded-[6px]`}>{tag}</p>
+            ))}
+            </div>
+            </div>
+      </div>
+    )  
+}
+
+export default AnimeCard
--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -11,6 +11,7 @@ import Plus from '/assets/PLus.png'
 import Info from '/assets/Info.png'
 import PlayBlack from '/assets/playblack.png'
 import animeCards from '../storeData/animeData.json'
+import AnimeCard from '../Components/animeCards'
 
 function Home() {
   return (
@@ -73,30 +74,8 @@ function Home() {
     <button className='text-white bg-gray-700 opacity-75 py-4 px-6 rounded-xl mr-3 cursor-pointer flex gap-2 hover:bg-white hover:text-black hover:opacity-100'> View All</button>
   </div>
   <div className='mx-6 flex gap-8 flex-wrap justify-between'>
-    {animeCards.map(({description, image, link, rating, seasons, tags, title, year, size}) => (
-      <div className={`bg-gray-700 lg:w-[${size}] h-[380px] rounded-2xl cursor-pointer relative group overflow-hidden flex flex-col justify-end pb-5`}>
-      <a href={link}> 
-        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center z-10 opacity-0 hover:opacity-100 transition-opacity">
-          <img src={PlayBlack} alt="Play" className='py-4 px-4 bg-white rounded-[100%]' />
-        </div>
-      </a>
-      <img src={image} alt="" className="w-full h-full absolute top-0 left-0 object-cover opacity-40 rounded-2xl group-hover:scale-110 transition-transform" /> 
-      <div className='relative px-6'>
-        <p className='text-3xl font-bold'>{title}</p>
-    <div className='flex items-center gap-4 mt-3 mb-4'>
-      
-        <p className='bg-gray-600 py-1 px-3 rounded-[6px] '>{rating}</p>
-        <p>{year}</p>
-        <p>{seasons} Seasons</p>
-        </div>
-        <p>{description}</p>
-        <div className='flex gap-1 mt-4'>
-          {tags.map((tag, index) => (
-            <p className={`${index === 0? "bg-red-700" : "bg-gray-600"} py-1 px-3 rounded-[6px]`}>{tag}</p>
-          ))}
-        </div>
-        </div>
-      </div>  
+    {animeCards.map((anime) => (
+      <AnimeCard anime={anime} />
     ))}
   </div>
 
@@ -453,6 +432,7 @@ function Home() {
   
 </section>
 {/* Recommended Section Ends */}
+
 
 {/* Footer Section Start */}
 <section>
